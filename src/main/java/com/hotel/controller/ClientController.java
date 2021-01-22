168770f32aefc00f9012dce8bfd1613669539142
Arkaliuk <viktor.ark22@gmail.com>
2021-01-22 16:52:01 +0200

    add: client: service, repository, controller

--- src/main/java/com/hotel/controller/ClientController.java
+++ src/main/java/com/hotel/controller/ClientController.java
@@ -1,11 +1,28 @@
 package com.hotel.controller;
 
+import com.hotel.model.Client;
+import com.hotel.service.ClientService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/client")
 public class ClientController {
+
+    private ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @CrossOrigin
+    @GetMapping
+    public List<Client> getAll() {
+        return clientService.getAll();
+    }
 }
--- src/main/java/com/hotel/service/ClientService.java
+++ src/main/java/com/hotel/service/ClientService.java
@@ -1,7 +1,21 @@
 package com.hotel.service;
 
+import com.hotel.model.Client;
+import com.hotel.repository.ClientRepository;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class ClientService {
+
+    private ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+
+        this.clientRepository = clientRepository;
+    }
+
+    public List<Client> getAll() {
+       return clientRepository.findAll();
+   }
 }
